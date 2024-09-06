1759e45307b007efdfd20fc0ef551d89e0387e6e
Karla G. <k.grajalesluna@gmail.com>
2024-09-06 19:56:04 +0100

    added the carousel in the Section

---
+++ script.js
@@ -0,0 +1,36 @@
+var gallery;
+
+gallery = [
+  'images/prado.jpg', 
+  'images/Botanico.jpg', 
+  'images/Deboh.jpg', 
+  'images/Palacio.jpg', 
+  'images/PlazaToros.jpg', 
+  'images/retiro.webp',
+  'images/retiro.avif',
+  'images/Cristal.jpg',
+  'images/teleferico.webp',
+  'images/pantano.jpg',
+  'images/cristalNoche.webp',
+  'images/madrid1.avif',
+  
+];
+
+let element_img_container = document.getElementById('img-container');
+element_img_container.setAttribute("src", gallery[0]);
+
+document.getElementById('next').addEventListener('click', (event) => {
+  let element_img_container2 = document.getElementById('img-container');
+  if (gallery.length != 0) {
+    gallery.push(gallery.shift());
+  }
+  element_img_container2.setAttribute("src", gallery[0]);
+});
+
+document.getElementById('prev').addEventListener('click', (event) => {
+  let element_img_container3 = document.getElementById('img-container');
+  element_img_container3.setAttribute("src", gallery.slice(-1)[0]);
+  if (gallery.length != 0) {
+    gallery.unshift(gallery.pop());
+  }
+});
